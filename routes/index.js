d44ff972276673aee04a57990b185ef5b14eb290
BigTimeCoffee <padraigdowling1@hotmail.com>
2021-12-01 13:58:23 -0300

    First 20 rendered

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express')
 const router = express.Router()
+const fetch = require('node-fetch')
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' })
+router.get('/', function (req, res) {
+    fetch('https://api.tvmaze.com/shows?page=1')
+        .then((result) => result.json())
+        .then((shows) => {
+            let firstTwenty = []
+            for (let i = 0; i < 20; i++) {
+                firstTwenty.push(shows[i])
+                console.log('These are the shows :', shows[i].name)
+            }
+            res.render('index', {
+                layout: 'layouts/index',
+                title: 'TV-API',
+                shows: firstTwenty,
+            })
+        })
+})
+
+router.get('/login', (req, res) => {
+    // if (app.locals.error) {
+    //     console.log(app.locals.error)
+    // }
+    res.render('login', { layout: 'layouts/login' })
+})
+
+router.get('/signup', (req, res) => {
+    res.render('signUp', { layout: 'layouts/signUp' })
 })
 
 module.exports = router
