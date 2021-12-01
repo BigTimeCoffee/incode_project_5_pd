d9bc252f41bb91082c700930b529bc17a30b3cc3
BigTimeCoffee <87614492+BigTimeCoffee@users.noreply.github.com>
2021-12-01 14:05:55 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
-const createError = require('http-errors')
 const express = require('express')
+const app = express()
 const path = require('path')
 const cookieParser = require('cookie-parser')
-const logger = require('morgan')
-
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const port = 4000
+const fetch = require('node-fetch')
 
-const app = express()
+require('dotenv').config()
+
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'incode_project_5_db',
+    port: 3306,
+})
+
+connection.connect(function (err) {
+    if (err) {
+        throw err
+    } else {
+        console.log('Connected to db')
+    }
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
-app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
@@ -22,20 +39,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404))
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-    // render the error page
-    res.status(err.status || 500)
-    res.render('error')
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
 })
 
 module.exports = app
